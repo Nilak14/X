8147a9f5a6ea6178f963399d0208c6e81948c746
Nilak Pathak <nilakpathak14@gmail.com>
2024-07-26 21:43:58 +0545

    db connection

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+export const signup = async (req, res) => {
+  res.json({ data: "You are in signup" });
+};
+
+export const login = async (req, res) => {
+  res.json({ data: "You are in login" });
+};
+export const logout = async (req, res) => {
+  res.json({ data: "You are in logout" });
+};
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("server is running");
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+dotenv.config();
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+  connectMongoDB();
+});
