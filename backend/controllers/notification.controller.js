b78dc2a5e4e8015822c6f161dea9f14531524116
Nilak Pathak <nilakpathak14@gmail.com>
2024-08-07 21:08:27 +0545

    notification api finish

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -21,7 +21,27 @@ export const deleteNotification = async (req, res) => {
     await Notification.deleteMany({ to: userId });
     res.status(200).json({ message: "Notification Deleted Successfully" });
   } catch (error) {
-    console.log(`Error in get deleteNotification ${error.message}`);
+    console.log(`Error in  deleteNotification ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const deleteSingleNotification = async (req, res) => {
+  try {
+    const { id: notificationId } = req.params;
+    const notification = await Notification.findById(notificationId);
+    if (!notification) {
+      return res.status(404).json({ error: "Notification Not Found" });
+    }
+    if (req.user._id.toString() !== notification.to.toString()) {
+      return res.status(401).json({ error: "Unauthorized Access" });
+    }
+    await Notification.findOneAndDelete({ _id: notificationId });
+    return res
+      .status(200)
+      .json({ message: "Notification Deleted Successfully" });
+  } catch (error) {
+    console.log(`Error in get deleteSingleNotification ${error.message}`);
     res.status(500).json({ error: error.message });
   }
 };
